d887a8133f6954579aa062935a8c5d151d177c6c
Eduardo Santos <e3duardo@live.com>
2020-12-18 22:13:00 -0300

    Registering GraphQL with Express*

--- section1/users/server.js
+++ section1/users/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const PORT = 4000;
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`🚀 Listening at ${PORT}...`);
