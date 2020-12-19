a2af9d3ee962b92964c7052f818a44fa65916436
Eduardo Santos <e3duardo@live.com>
2020-12-18 23:44:43 -0300

    Resolving with Data

--- section1/users/schema/schema.js
+++ section1/users/schema/schema.js
@@ -1,5 +1,10 @@
-const graphql,
-  { GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = require("graphql");
+const _ = require("lodash");
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -8,4 +13,34 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const users = [
+  {
+    id: "1",
+    firstName: "Eduardo Santos",
+    age: 30,
+  },
+  {
+    id: "2",
+    firstName: "Jaqueline Ferreira",
+    age: 30,
+  },
+];
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
