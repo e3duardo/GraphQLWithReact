a77de117691972bbb836248d9db49f4780adf37a
Eduardo Santos <e3duardo@live.com>
2020-12-18 22:32:34 -0300

    Writing a GraphQL Schema

---
+++ section1/users/schema/schema.js
@@ -0,0 +1,11 @@
+const graphql,
+  { GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
