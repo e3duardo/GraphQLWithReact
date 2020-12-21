fe246ae51032b2136e7a573cf0ef08c26f76baf1
Eduardo Santos <e3duardo@live.com>
2020-12-21 18:21:20 -0300

    Bonding Queries with Components

--- section6/lyrical/client/components/SongList.js
+++ section6/lyrical/client/components/SongList.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
-function SongList() {
-  return <div>SongList</div>;
+class SongList extends Component {
+  render() {
+    const { data } = this.props;
+
+    if (data.loading) {
+      return <p>Loading...</p>;
+    }
+    return (
+      <ul>
+        {data.songs.map((s) => (
+          <li key={s.id}>{s.title}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
-export default SongList;
+const query = gql`
+  query {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
