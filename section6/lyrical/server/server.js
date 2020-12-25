8ebea66bd4f3eaaff91cdb237ab3a776c1aa90ed
Eduardo Santos <e3duardo@live.com>
2020-12-25 11:57:39 -0300

    Changing Docker settings

--- section6/lyrical/server/server.js
+++ section6/lyrical/server/server.js
@@ -9,13 +9,13 @@ const cors = require("cors");
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = "mongodb://lyric:password@localhost:27017";
+const MONGO_URI = "mongodb://lyrical:password@localhost:27017/lyrical-db";
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoLab URI");
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useMongoClient: true /* other options */ });
 mongoose.connection
   .once("open", () => console.log("Connected to MongoLab instance."))
   .on("error", (error) => console.log("Error connecting to MongoLab:", error));
