52ba6b765d31b28ba72f101aac27219eb561f862
Eduardo Santos <e3duardo@live.com>
2020-12-29 09:31:19 -0300

    Including Cookies with GraphQL Requests

--- section12/auth/client/components/Header.js
+++ section12/auth/client/components/Header.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+
+import query from "../queries/CurrentUser";
 
 class Header extends Component {
   render() {
+    if (this.props.data.loading) {
+      return null;
+    }
+
+    if (this.props.data.user) {
+      return <div>user</div>;
+    }
+
     return <div>Header</div>;
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
