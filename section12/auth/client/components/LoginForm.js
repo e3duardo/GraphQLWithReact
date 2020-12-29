2b1c041da4c0d0f59696e81651d7bfb5b81cb54e
Eduardo Santos <e3duardo@live.com>
2020-12-29 11:05:13 -0300

    Error Handling with GraphQL

--- section12/auth/client/components/LoginForm.js
+++ section12/auth/client/components/LoginForm.js
@@ -6,21 +6,33 @@ import mutation from "../mutations/Login";
 import AuthForm from "./AuthForm";
 
 class LoginForm extends Component {
-  onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: {
-        email,
-        password,
-      },
-      refetchQueries: [{ query }],
+  constructor(props) {
+    super(props);
+    this.state({
+      errors: [],
     });
+  }
+  onSubmit({ email, password }) {
+    this.props
+      .mutate({
+        variables: {
+          email,
+          password,
+        },
+        refetchQueries: [{ query }],
+      })
+      .catch((res) => {
+        const errors = res.graphQLErrors.map((error) => error.message);
+
+        this.setState({ errors });
+      });
   }
 
   render() {
     return (
       <div>
         <h3>Login</h3>
-        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm onSubmit={this.onSubmit.bind(this)} errors={errors} />
       </div>
     );
   }
